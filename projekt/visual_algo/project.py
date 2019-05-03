61d5df08118fbd6d5f2ae0d7ec61aee508999175
mw <marcin.wierzbinski@outlook.com>
2019-05-03 22:18:15 +0200

    modify

--- projekt/visual_algo/project.py
+++ projekt/visual_algo/project.py
@@ -10,7 +10,6 @@ G.add_edges_from(
      ('D', 'B'),
      ('E', 'C'),
      ('E', 'F'),
-     ('B', 'H'),
      ('B', 'G'),
      ('B', 'F'),
      ('C', 'G')])
@@ -18,32 +17,31 @@ G.add_edges_from(
 
 graph1 = {
     'A': ['B', 'C'],
-    'B': ['H', 'G', 'F'],
+    'B': ['G', 'F'],
     'C': ['G'],
     'D': ['B'],
     'E': ['C', 'F'],
     'F': [],
     'G': [],
-    'H': [],
 }
 
 visited = []
+fig = plt.figure(1, figsize=(8, 8))
+txt = fig.text(.05, .9, ' DFS', fontsize=15)
 
 
-val_map = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+val_map = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
 val_map_c = {'A': 0.75}
 
 values = [val_map_c.get(node, 0.25) for node in G.nodes()]
 
-# Specify the edges you want here
-red_edges = [('A', 'C'), ('E', 'C')]
 
 # Need to create a layout when doing
 # separate calls to draw nodes and edges
 pos = nx.spring_layout(G)
-print(values)
 
-nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_color = values, node_size = 500)
+
+nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_color=values, node_size=500)
 nx.draw_networkx_labels(G, pos)
 nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrows=True)
 
@@ -51,13 +49,13 @@ nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrows=True)
 plt.show()
 plt.pause(1)
 
-print("wykonal sie")
+
 def draw_nodes(node):
     plt.pause(3)
     index = val_map[node]
     values[index] = 0.75
     nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_color=values, node_size=500)
-draw_nodes('D')
+
 
 def dfs(graph, node):
     global visited
@@ -69,10 +67,6 @@ def dfs(graph, node):
 
 
 dfs(graph1, 'A')
-
-
-plt.pause(1)
-
 
 plt.pause(100)
 plt.show()
